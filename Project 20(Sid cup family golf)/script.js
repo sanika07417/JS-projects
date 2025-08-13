71522fb4ca30e198d5ee60f6a1e1063a4e1c47a3
Sanika Chougule <chougulesanika50@gmail.com>
2025-08-13 23:45:50 +0530

    commit

--- Project 20(Sid cup family golf)/script.js
+++ Project 20(Sid cup family golf)/script.js
@@ -2,10 +2,10 @@ var crsr=document.querySelector("#cursor");
 var crsrBlur=document.querySelector("#cursor-blur");
 
 document.addEventListener("mousemove",function(dets){
-    crsr.style.left=dets.x+"px"
+    crsr.style.left=dets.x+30+"px"
     crsr.style.top=dets.y+"px"
-    crsrBlur.style.left=dets.x-220+"px"
-    crsrBlur.style.top=dets.y-220+"px"
+    crsrBlur.style.left=dets.x-250+"px"
+    crsrBlur.style.top=dets.y-250+"px"
 })
 
 
@@ -36,3 +36,80 @@ gsap.to("#container",{
         scrub:2,
     }
 })
+var h4all=document.querySelectorAll("#nav h4");
+h4all.forEach(function(elem){
+    elem.addEventListener("mouseenter",function(){
+        crsr.style.scale=3;
+        crsr.style.border="1px solid #fff";
+        crsr.style.backgroundColor="transparent";
+    })
+
+    elem.addEventListener("mouseleave",function(){
+        crsr.style.scale=1;
+        crsr.style.border="0px solid #95c11e";
+        crsr.style.backgroundColor="transparent";
+    })
+})
+
+gsap.from("#about-us img, #about-us-in", {
+    y: 50,
+    opacity:0,
+    duration: 1, 
+    scrollTrigger: {
+      trigger: "#about-us",
+      scroller: "body",
+      start: "top 70%",
+      end: "top 65%",
+      scrub: 1,
+ },
+});
+
+gsap.from(".card", {
+    scale: 0.8,
+    opacity:0,
+    duration: 1,
+    stagger: 0.1,
+    scrollTrigger: {
+        trigger: ".card",
+        scroller: "body",
+        start: "top 70%",
+        end: "top 65%",
+        scrub: 1,
+    },
+});
+
+
+gsap.from("#colon1", {
+    y: -70,
+    x: -70,
+    scrolltrigger: {
+        trigger: "#colon1",
+        scroller: "body",
+        start: "top 55%",
+        end: "top 45%",
+        scrub: 1,
+    },
+});
+
+gsap.from("#colon2", {
+    y: 70,
+    x: 70,
+    scrolltrigger: {
+        trigger: "#colon1",
+        scroller: "body",
+        start: "top 55%",
+        end: "top 45%",
+        scrub: 4,
+    },
+})
+
+gsap.from("#page4 h1", {
+    y: 50,
+    scrollerTrigger: {
+        trigger: "#page4 h1",
+        scroller:"body",
+        start: "top 75%",
+        end: "top 70%",
+        scrub: 3,
+    },
+});
